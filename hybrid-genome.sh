a80edd73fc7549a9a7db4b93ecfdff4536f8b45b
Roth E Conrad <rotheconrad@ufl.edu>
2017-05-11 15:38:17 -0400

    Update hybrid-genome.sh

--- hybrid-genome.sh
+++ hybrid-genome.sh
@@ -6,20 +6,20 @@
 
 module load samtools
 
-Sorted=`ls *.sam | head -n $SLURM_ARRAY_TASK_ID | tail -n 1`
-Sorted_out=`echo $Sorted | cut -d. -f1-3`.mpileup.vcf
+Sorted=filename.sam #Name of the samfile output by tophat or hisat or gsnap or star or whatever. The bam file should probably be sorted.
+Sorted_out=${Sorted}.mpileup.vcf
 
-samtools mpileup -u -B -v -q 30 -f Ceratodon_purpureus.main_genome.scaffolds.sorted.fa -o $Sorted_out $Sorted.sorted.bam
+samtools mpileup -u -B -v -q 30 -f Ceratodon_purpureus.main_genome.scaffolds.sorted.fa -o $Sorted_out $Sorted
 
 module load bcftools
 
-bcf_out=`echo $Sorted | cut -d. -f1-3`.bcfcalls.vcf
+bcf_out=${Sorted}.bcfcalls.vcf
 
 bcftools call -m -V indels -o $bcf_out $Sorted_out
 
 module load gatk
 
-GATK_out=`echo $Sorted | cut -d. -f1-2`.hybrid_genome.fasta
+GATK_out=${Sorted}.hybrid_genome.fasta
 
 java -jar $HPC_GATK_DIR/GenomeAnalysisTK.jar -T FastaAlternateReferenceMaker -R Ceratodon_purpureus.main_genome.scaffolds.sorted.fa -V $bcf_out -o $GATK_out
 
